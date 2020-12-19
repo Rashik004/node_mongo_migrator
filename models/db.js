51e819f9adcbfd8f51ab9bbaccef6751220b2ce3
Rashik Hasnat <rhasnat93@gmail.com>
2020-12-20 01:06:49 +0600

    misc.

--- models/db.js
+++ models/db.js
@@ -14,7 +14,7 @@ client.connect(err => {
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
